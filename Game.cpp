9020381c1ffe9a20884a1abcd3371c9c5a7f4a9f
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2024-12-24 15:25:08 -0300

    kdevelop gitignore

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 #include "Game.hpp"
 
 Game::Game(){
@@ -11,6 +11,10 @@ Game::~Game(){
 
 void Game::init(const char* title, int xpos, int ypos,int width, int height, bool fullscreen){
 
+    if(window->isOpen()){
+        std::cout<<"Window created!"<<std::endl;
+        isRunning = true;
+    }
 }
 
 void Game::handleEvents(){
