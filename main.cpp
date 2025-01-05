f09ba4653bb62d6b956788d5b6d04e13aebe7f13
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-01-05 19:31:11 -0300

    melhoria no gerador de tilemap

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Game.hpp"
+#include "TileGenerator.hpp"
 
 int main(int argc, char **argv) {
 
     Game game;
     game.run(60);
 
+    TileGenerator tileGen;
+    tileGen.generateTileMap("/home/jacques/Downloads/env-game.png","/home/jacques/Downloads/tileMapNumbers.txt",32,32);
     return 0;
 }
