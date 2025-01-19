498e87319961d36f61fc13dde4946c7c29aca087
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-01-18 21:43:39 -0300

    implementação MainMenuState.hpp

--- UI/MainMenuState.hpp
+++ UI/MainMenuState.hpp
@@ -1,9 +1,23 @@
 #ifndef MAIN_MENU_STATE_HPP
 #define MAIN_MENU_STATE_HPP
 
-class MainMenuSate{
+#include "GameState.hpp"
+#include <vector>
+#include <string>
+
+class MainMenuSate : public GameState{
 public:
+    MainMenuSate();
+    ~MainMenuSate() override = default;
 
 private:
+    sf::Font font;
+    sf::Text title;
+    std::vector<sf::Text> options;
+    int selectedOption;
+    bool continueGame;
+    bool exitGame;
+
+    void updateOptionColors();
 };
 #endif
