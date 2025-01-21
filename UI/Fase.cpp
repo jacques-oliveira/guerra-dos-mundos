826c03b7cb394ab3839ddadcc19695ec7c63929b
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-01-21 11:16:17 -0300

    adapetando Fase abstrata em Fase1...

---
+++ UI/Fase.cpp
@@ -0,0 +1,67 @@
+#include "Fase.hpp"
+
+constexpr float SIZE(1024.0f);
+
+Fase::Fase(const std::string& levelName) : levelCompleted(false), exitGame(false){
+
+}
+
+Fase::~Fase(){
+
+}
+void Fase::startSelection(sf::Vector2f& start){
+    isSelectingPlayer = true;
+    selectionStart = start;
+    selectionBox.setPosition(start);
+    selectionBox.setSize({0.f,0.f});
+}
+
+void Fase::configureSelectionBox(){
+    selectionBox.setFillColor(sf::Color(0, 0, 255, 25)); // Azul translúcido
+    selectionBox.setOutlineThickness(1.f);
+    selectionBox.setOutlineColor(sf::Color::Blue);
+    isSelectingPlayer = false;
+}
+
+void Fase::updateSelection(const sf::Vector2f& current){
+    sf::Vector2f size = current - selectionStart;
+    selectionBox.setSize(size);
+    selectionBox.setPosition({
+        min(selectionStart.x,  current.x),
+                             min(selectionStart.y,current.y)
+    });
+}
+
+void Fase::endSelection(){
+    isSelectingPlayer = false;
+    sf::FloatRect selectionArea(
+        selectionBox.getPosition(),
+                                selectionBox.getSize()
+    );
+    player->setSelected(player->isInside(selectionArea));
+}
+
+void Fase::moveSelectedPlayers(sf::Vector2f& dest){
+    player->setDestination(dest);
+}
+
+void Fase::updateViewSize(sf::View& view, sf::RenderWindow& _window){
+    float ratio = (float)_window.getSize().y / (float)_window.getSize().x;
+    view.setSize(SIZE, SIZE * ratio);
+}
+
+void Fase::configureTileMap(){
+    char path[100] =  {"Assets/Textures/forest.png\0"};
+    tileGen = new TileGenerator();
+    tileGen->generateTileMap(path);
+}
+
+bool Fase::shouldContinue() const {
+    return levelCompleted;
+}
+
+bool Fase::shouldExit() const {
+    return exitGame;
+}
+
+
