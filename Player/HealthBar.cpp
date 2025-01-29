082cf2a142dfb30c267991c8bc572c50944fd788
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-01-29 13:51:48 -0300

    assnatura HealthBar.cpp

---
+++ Player/HealthBar.cpp
@@ -0,0 +1,22 @@
+#include "HeathBar.hpp"
+
+HealthBar::HealthBar(float width, float heigth){
+
+}
+
+HealthBar::~HealthBar(){
+}
+
+void HealthBar::setPosition(float x, float y){
+}
+
+void HealthBar::setHealth(float healthValue){
+}
+
+void HealthBar::draw(sf::RenderWindow& window){
+}
+
+
+
+
+
