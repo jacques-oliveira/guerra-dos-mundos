66d9c08b7c1a256ece82ff31d740dda98736cbf8
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-01-29 16:02:28 -0300

    healbar.cpp

--- Player/HealthBar.cpp
+++ Player/HealthBar.cpp
@@ -1,19 +1,28 @@
 #include "HeathBar.hpp"
 
 HealthBar::HealthBar(float width, float heigth){
+    background.setSize(sf::Vector2f(width, heigth));
+    background.setFillColor(sf::Color::Black);
 
+    healthBar.setSize(sf::Vector2f(width,heigth));
+    healthBar.setFillColor(sf::Color::Green);
 }
 
 HealthBar::~HealthBar(){
 }
 
 void HealthBar::setPosition(float x, float y){
+    background.setPosition(x,y);
+    healthBar.setPosition(x,y);
 }
 
 void HealthBar::setHealth(float healthValue){
+    healthBar.setSize(sf::Vector2f(background.getSize().x * healthValue, background.getSize().y));
 }
 
 void HealthBar::draw(sf::RenderWindow& window){
+    window.draw(background);
+    window.draw(healthBar);
 }
 
 
