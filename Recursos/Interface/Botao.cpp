c057eb2ae62dc1a640b3ea6c1ad26d7ee4a87a91
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-09 17:02:17 -0300

    implementando botao menu

--- Recursos/Interface/Botao.cpp
+++ Recursos/Interface/Botao.cpp
@@ -1,1 +1,34 @@
 #include "Botao.hpp"
+
+Botao::Botao(float width, float height, const sf::Texture& texturaNormal, const sf::Texture& texturaSelecaoMouse, const sf::Texture& texturaSelecioando)
+{
+    formaBotao.setSize(sf::Vector2f(width, height));
+    formaBotao.setTexture(&texturaNormal);
+
+    this->texturaNormal = &texturaNormal;
+    this->texturaSelecaoMouse = &texturaSelecaoMouse;
+    this->texturaSelecioando = &texturaSelecioando;
+}
+
+void Botao::atualizar(sf::RenderWindow& window){
+    sf::Vector2i posicaoMouse = sf::Mouse::getPosition(window);
+    sf::FloatRect limites = formaBotao.getGlobalBounds();
+
+    if(limites.contains(posicaoMouse.x, posicaoMouse.y)){
+        formaBotao.setTexture(texturaSelecaoMouse);
+    }else{
+        formaBotao.setTexture(texturaNormal);
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) && limites.contains(posicaoMouse.x, posicaoMouse.y)){
+        formaBotao.setTexture(texturaSelecioando);
+    }
+
+}
+
+void Botao::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    states.transform *= getTransform();
+    target.draw(formaBotao,states);
+}
+
+
--- Recursos/Interface/Botao.hpp
+++ Recursos/Interface/Botao.hpp
@@ -8,6 +8,7 @@ public:
     Botao(float width, float height, const sf::Texture& texturaNormal, const sf::Texture& texturaSelecaoMouse, const sf::Texture& texturaSelecioando);
     ~Botao();
     void atualizar(sf::RenderWindow& window);
+
 private:
     virtual void draw(sf::RenderTarget & target, sf::RenderStates states) const override;
     sf::RectangleShape formaBotao;
