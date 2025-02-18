cd7e52a296783ecc54bccf9b2a0b614cf02fa8a5
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-18 08:03:17 -0300

    implementando fonteEnergia

--- GeradorMapa/FonteEnergia.cpp
+++ GeradorMapa/FonteEnergia.cpp
@@ -1,7 +1,9 @@
 #include "FonteEnergia.hpp"
 
-FonteEnergia::FonteEnergia(float posx, float posy){
+FonteEnergia::FonteEnergia(float posx, float posy, int energiaFonte){
     posicao = sf::Vector2f(posx, posy);
+    this->energiaFonte = energiaFonte;
+    iniciar();
 
 }
 
@@ -9,8 +11,33 @@ void FonteEnergia::iniciar(){
     if(!texturaCristalNegro.loadFromFile("Assets/Textures/textura-cristal-negro.png")){
         std::cerr<<"Falha ao carregar textura cristal negro"<<std::endl;
     }
-    spriteCristalNegro.setPosition(posicao);
-    spriteCristalNegro.setTexture(texturaCristalNegro);
+
+    animacoes.resize(1);
+    animacoes[0].setSpriteSheet(texturaCristalNegro);
+    carregarAnimacao(animacoes[0], 8, 0);
+
+    spriteCristalNegro.setTexture(animacoes[0].getTexture());
+    spriteCristalNegro.setTextureRect(animacoes[0].getCurrentFrame());
 }
+
+void FonteEnergia::carregarAnimacao(Animation& animacao, int quantidadeFrames, int linha)
+{
+
+        for(int i = 0; i < quantidadeFrames; ++i){
+            animacao.addFrame(sf::IntRect(i * 256, linha * 256, 256,256));
+        }
+}
+
+void FonteEnergia::atualizar(){
+    animacoes[0].update();
+    spriteCristalNegro.setTextureRect(animacoes[0].getCurrentFrame());
+}
+
+void FonteEnergia::renderizar(sf::RenderWindow& window){
+    window.draw(spriteCristalNegro);
+}
+
+
+
 
 
