ab21ed2a6edb7edd3915e7ff0eace042fd1f4e58
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-22 19:45:58 -0300

    finalizando barra mensagem

--- Recursos/BarraMensagem/BarraMensagem.cpp
+++ Recursos/BarraMensagem/BarraMensagem.cpp
@@ -3,15 +3,22 @@
 BarraMensagem::BarraMensagem(){
     try{
         tituloMensagem.setString("Mensagem: ");
-        stringMensagem = "Sua missão começou, nos orgulhe!";
-        tituloMensagem.setPosition(200,100);
-        textMensagem.setPosition(250,100);
+        stringMensagem = L"Sua missão começou, nos orgulhe ! . . .";
+        csEsquerdo.setFillColor(sf::Color::Black);
+        csDireito.setFillColor(sf::Color::Black);
+        retanguloFundo.setFillColor(sf::Color::Black);
+        tamanhoFonte = 20.f;
+
     }catch(std::exception& e){
         std::cerr<<"Falha ao carregar barra de mensagem"<<e.what()<<std::endl;
     }
 }
 
 void BarraMensagem::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    target.draw(csEsquerdo,sf::RenderStates::Default);
+    target.draw(retanguloFundo,sf::RenderStates::Default);
+    target.draw(csDireito,sf::RenderStates::Default);
+
     target.draw(tituloMensagem,sf::RenderStates::Default);
     target.draw(textMensagem,sf::RenderStates::Default);
 }
@@ -25,10 +32,30 @@ void BarraMensagem::atribuirFonte(sf::Font& fonte){
         textMensagem.setFont(fonte);
         tituloMensagem.setFont(fonte);
         textMensagem.setString(stringMensagem);
+        textMensagem.setFillColor(sf::Color::Yellow);
+        tituloMensagem.setCharacterSize(tamanhoFonte);
+        textMensagem.setCharacterSize(tamanhoFonte);
     }catch(std::exception& e){
         std::cerr<<"Falha ao atriubuir Fonte a Barra de Mensagem"<<e.what()<<std::endl;
     }
 }
 
+void BarraMensagem::atribuirPosicao(float posx, float posy, float tamanhoRetangulo){
+    try{
+        csEsquerdo.setRadius(15);
+        csDireito.setRadius(15);
+        retanguloFundo.setSize(sf::Vector2f(tamanhoRetangulo*0.45f,csEsquerdo.getRadius() * 2));
 
-
+        csEsquerdo.setPosition(posx,posy);
+        retanguloFundo.setPosition(posx + csEsquerdo.getRadius(), posy);
+        csDireito.setPosition(csEsquerdo.getPosition().x + retanguloFundo.getGlobalBounds().width, posy);
+        std::cout<<retanguloFundo.getGlobalBounds().width<<std::endl;
+        tituloMensagem.setOrigin(tituloMensagem.getLocalBounds().left, tituloMensagem.getLocalBounds().height/2);
+        tituloMensagem.setPosition(posx + csEsquerdo.getRadius(),
+                                   posy + tamanhoFonte/2);
+        textMensagem.setOrigin(textMensagem.getLocalBounds().left, textMensagem.getLocalBounds().height/2);
+        textMensagem.setPosition(posx + tituloMensagem.getGlobalBounds().width + 24.f, tituloMensagem.getPosition().y);
+    }catch(std::exception& e){
+        std::cerr<<"Falha ao atriubuir posição a Barra de Mensagem"<<e.what()<<std::endl;
+    }
+}
--- Recursos/BarraMensagem/BarraMensagem.hpp
+++ Recursos/BarraMensagem/BarraMensagem.hpp
@@ -9,12 +9,16 @@ class BarraMensagem : sf::Drawable{
 public:
     BarraMensagem();
     virtual ~BarraMensagem() = default;
-    std::string stringMensagem;
+    std::wstring stringMensagem;
     void render(sf::RenderWindow& window);
     void atribuirFonte(sf::Font& fonte);
+    void atribuirPosicao(float posx, float posy, float tamanhoRetangulo);
     sf::Text textMensagem;
     sf::Text tituloMensagem;
 private:
     void draw(sf::RenderTarget & target, sf::RenderStates states) const override;
+    sf::CircleShape csEsquerdo, csDireito;
+    sf::RectangleShape retanguloFundo;
+    float tamanhoFonte;
 };
 #endif
