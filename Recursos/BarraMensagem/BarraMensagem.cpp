cd8b8b8e71ce0d8474c1e2a4c278985c94b7c317
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-22 11:39:53 -0300

    carregando barra de mensagem

--- Recursos/BarraMensagem/BarraMensagem.cpp
+++ Recursos/BarraMensagem/BarraMensagem.cpp
@@ -1,17 +1,34 @@
 #include "BarraMensagem.hpp"
 
 BarraMensagem::BarraMensagem(){
-    tituloMensagem.setString("Mensagem: ");
-    textoMensagem = "Sua missão começou, nos orgulhe!";
+    try{
+        tituloMensagem.setString("Mensagem: ");
+        stringMensagem = "Sua missão começou, nos orgulhe!";
+        tituloMensagem.setPosition(200,100);
+        textMensagem.setPosition(250,100);
+    }catch(std::exception& e){
+        std::cerr<<"Falha ao carregar barra de mensagem"<<e.what()<<std::endl;
+    }
 }
 
 void BarraMensagem::draw(sf::RenderTarget& target, sf::RenderStates states) const{
     target.draw(tituloMensagem,sf::RenderStates::Default);
-    target.draw(mensagem,sf::RenderStates::Default);
+    target.draw(textMensagem,sf::RenderStates::Default);
 }
 
 void BarraMensagem::render(sf::RenderWindow& window){
     draw(window, sf::RenderStates::Default);
 }
+
+void BarraMensagem::atribuirFonte(sf::Font& fonte){
+    try{
+        textMensagem.setFont(fonte);
+        tituloMensagem.setFont(fonte);
+        textMensagem.setString(stringMensagem);
+    }catch(std::exception& e){
+        std::cerr<<"Falha ao atriubuir Fonte a Barra de Mensagem"<<e.what()<<std::endl;
+    }
+}
+
 
 
--- Recursos/BarraMensagem/BarraMensagem.hpp
+++ Recursos/BarraMensagem/BarraMensagem.hpp
@@ -1,7 +1,7 @@
 #ifndef BARRA_MENSAGEM_HPP
 #define BARRA_MENSAGEM_HPP
 
-#include <istream>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 class BarraMensagem : sf::Drawable{
@@ -9,11 +9,12 @@ class BarraMensagem : sf::Drawable{
 public:
     BarraMensagem();
     virtual ~BarraMensagem() = default;
-    std::string textoMensagem;
+    std::string stringMensagem;
     void render(sf::RenderWindow& window);
+    void atribuirFonte(sf::Font& fonte);
+    sf::Text textMensagem;
+    sf::Text tituloMensagem;
 private:
     void draw(sf::RenderTarget & target, sf::RenderStates states) const override;
-    sf::Text mensagem;
-    sf::Text tituloMensagem;
 };
 #endif
