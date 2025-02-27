d045c3d925469566f061ecef2622f1bba78b5993
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-27 09:45:25 -0300

    atribuição de fonte e posição do bloco do painel

--- Interface/PainelControle/BlocoItemPainel.cpp
+++ Interface/PainelControle/BlocoItemPainel.cpp
@@ -22,6 +22,8 @@ unsigned short BlocoItemPainel::obterQuantidadeItem(){
 void BlocoItemPainel::draw(sf::RenderTarget& target, sf::RenderStates states) const{
     target.draw(spriteBloco, states);
     target.draw(spriteItem);
+    target.draw(textQuantidadeItem);
+    target.draw(textValorItem);
 }
 
 void BlocoItemPainel::renderizar(sf::RenderWindow& window){
@@ -37,8 +39,12 @@ void BlocoItemPainel::carregarRecursos(){
         if(!texturaBloco.loadFromFile("Recursos/Textures/bloco_item.png")){
             std::cerr<<"Falha ao carregar textura no Bloco no painel"<<std::endl;
         }
+        if(!fonte.loadFromFile("Recursos/Fonts/TrulyMadlyDpad-a72o.ttf")){
+            std::cerr<<"Falha ao carregar fonte Truly"<<std::endl;
+        }
         spriteBloco.setTexture(texturaBloco);
         spriteBloco.setOrigin(spriteBloco.getLocalBounds().left, spriteBloco.getLocalBounds().height/2);
+        atribuirFonte(fonte);
     }catch(std::exception& e){
         std::cerr<<"Falha ao carregar recursos no bloco"<<e.what()<<std::endl;
     }
@@ -48,6 +54,17 @@ void BlocoItemPainel::atribuirFonte(sf::Font& fonte){
     try{
         textQuantidadeItem.setFont(fonte);
         textValorItem.setFont(fonte);
+
+        textQuantidadeItem.setCharacterSize(18);
+        textValorItem.setCharacterSize(20);
+
+        textQuantidadeItem.setFillColor(sf::Color::Yellow);
+        textValorItem.setFillColor(sf::Color::Green);
+
+        textValorItem.setString("900");
+        textQuantidadeItem.setString(std::to_string(quantidadeItem));
+        textValorItem.setOrigin(textValorItem.getLocalBounds().left, textValorItem.getLocalBounds().height/2);
+        textQuantidadeItem.setOrigin(textQuantidadeItem.getLocalBounds().left, textQuantidadeItem.getLocalBounds().height/2);
     }catch(std::exception& e){
         std::cerr<<"Erro ao atribuir fonte ao bloco"<<e.what()<<std::endl;
     }
@@ -56,11 +73,13 @@ void BlocoItemPainel::atribuirFonte(sf::Font& fonte){
 void BlocoItemPainel::atribuirPosicao(float posx, float posy){
     try{
         spriteBloco.setPosition(posx, posy);
-        float posxQuantidadeItem = spriteBloco.getGlobalBounds().left + spriteBloco.getGlobalBounds().width/2 -10.f;
-        float posyQuantidadeItem = spriteBloco.getGlobalBounds().top + spriteBloco.getGlobalBounds().height - 10.f;
+
+        float posxQuantidadeItem = spriteBloco.getGlobalBounds().left + spriteBloco.getGlobalBounds().width - textQuantidadeItem.getGlobalBounds().width -5.f;
+        float posyQuantidadeItem = spriteBloco.getGlobalBounds().top + spriteBloco.getGlobalBounds().height - textQuantidadeItem.getGlobalBounds().height - 5.f;
         textQuantidadeItem.setPosition(posxQuantidadeItem, posyQuantidadeItem);
+
         float posxValorItem = spriteBloco.getGlobalBounds().left + spriteBloco.getGlobalBounds().width/2 -10.f;
-        float posyValorItem = spriteBloco.getGlobalBounds().top + spriteBloco.getGlobalBounds().height - 10.f;
+        float posyValorItem = spriteBloco.getGlobalBounds().top + spriteBloco.getGlobalBounds().height + 2.f;
         textValorItem.setPosition(posxValorItem, posyValorItem);
     }catch(std::exception& e){
         std::cerr<<"Erro ao atribuir posição ao bloco"<<e.what()<<std::endl;
