8c943fa38b03843ba5510344633afca48acf85a0
Jacques Oliveira <130871640+jacques-oliveira@users.noreply.github.com>
2025-02-28 08:22:02 -0300

    ajustando bloco item a receber gerenciador

--- Interface/PainelControle/BlocoItemPainel.hpp
+++ Interface/PainelControle/BlocoItemPainel.hpp
@@ -6,13 +6,14 @@
 
 class BlocoItemPainel : public sf::Drawable{
 public:
-    BlocoItemPainel();
+    BlocoItemPainel(unsigned short valorItem);
     virtual ~BlocoItemPainel() = default;
 
     unsigned short obterValorItem();
     unsigned short obterQuantidadeItem();
     void atribuirPosicao(float posx, float posy);
     void atribuirFonte(sf::Font& fonte);
+
 private:
     void inicializar();
     void draw(sf::RenderTarget & target, sf::RenderStates states) const override;
